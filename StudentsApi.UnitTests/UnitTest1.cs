5841bbe425fd0544040c86addf5c0552fb0f7a45
angel leon <38294654+C0der97@users.noreply.github.com>
2023-11-18 13:09:47 -0500

    Se finaliza clase bussiness y inyección de dependencias, se agregan pruebas unitarias iniciales

---
+++ StudentsApi.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace StudentsApi.UnitTests;
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+              // Arrange
+        var mockSqlLiteProvider = new Mock<ISqlLiteProvider>();
+    Assert.False(false);
+    }
+}
